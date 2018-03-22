bdea565005aa18991c23e221e3a614e7eb008c57
Matthew Mckenzie <matthew.m.mckenzie@gmail.com>
2018-03-22 20:05:14 +1000

    attempting to fake it

--- test/specs/object-source-with-id/object-source-with-id.spec.js
+++ test/specs/object-source-with-id/object-source-with-id.spec.js
@@ -1,19 +1,17 @@
-describe('Object sources (instead of file paths)', function () {
+describe('Object sources with id set for reference', function () {
   'use strict';
 
   it('should dereference a single object', function () {
     var parser = new $RefParser();
     return parser
-      .dereference(helper.cloneDeep(helper.parsed.internal))
+      .dereference(helper.cloneDeep(helper.parsed.objectSourceWithIdInternal))
       .then(function (schema) {
-        console.log('paths', parser.$refs);
-
         expect(schema).to.equal(parser.schema);
-        expect(schema).to.deep.equal(helper.dereferenced.internal);
+        expect(schema).to.deep.equal(helper.dereferenced.objectSourceWithIdInternal);
 
         // The schema path should be the current directory
         var expectedPaths = [
-          path.cwd()
+          'http://www.example.com/',
         ];
         expect(parser.$refs.paths()).to.have.same.members(expectedPaths);
         expect(parser.$refs.values()).to.have.keys(expectedPaths);
@@ -31,15 +29,16 @@ describe('Object sources (instead of file paths)', function () {
   it('should dereference an object that references external files', function () {
     var parser = new $RefParser();
     return parser
-      .dereference(helper.cloneDeep(helper.parsed.objectSource.schema))
+      .dereference(helper.cloneDeep(helper.parsed.objectSourceWithId.schema))
       .then(function (schema) {
+        
         expect(schema).to.equal(parser.schema);
-        expect(schema).to.deep.equal(helper.dereferenced.objectSource);
-
+        expect(schema).to.deep.equal(helper.dereferenced.objectSourceWithId);
+        console.log('AAAAAAAAAAAAAAAAA', { schema: schema, paths: parser.$refs.paths() });
         // The schema path should be the current directory, and all other paths should be absolute
         var expectedPaths = [
-          path.cwd(),
-          path.abs('specs/object-source/definitions/definitions.json'),
+          'http://www.example.com/',
+          path.abs('1specs/object-source/definitions/definitions.json'),
           path.abs('specs/object-source/definitions/name.yaml'),
           path.abs('specs/object-source/definitions/required-string.yaml')
         ];
@@ -59,23 +58,23 @@ describe('Object sources (instead of file paths)', function () {
       });
   });
 
-  it('should bundle an object that references external files', function () {
-    var parser = new $RefParser();
-    return parser
-      .bundle(helper.cloneDeep(helper.parsed.objectSource.schema))
-      .then(function (schema) {
-        expect(schema).to.equal(parser.schema);
-        expect(schema).to.deep.equal(helper.bundled.objectSource);
+  // it('should bundle an object that references external files', function () {
+  //   var parser = new $RefParser();
+  //   return parser
+  //     .bundle(helper.cloneDeep(helper.parsed.objectSourceWithIdInternal.schema))
+  //     .then(function (schema) {
+  //       expect(schema).to.equal(parser.schema);
+  //       expect(schema).to.deep.equal(helper.bundled.objectSourceWithIdInternal);
 
-        // The schema path should be the current directory, and all other paths should be absolute
-        var expectedPaths = [
-          path.cwd(),
-          path.abs('specs/object-source/definitions/definitions.json'),
-          path.abs('specs/object-source/definitions/name.yaml'),
-          path.abs('specs/object-source/definitions/required-string.yaml')
-        ];
-        expect(parser.$refs.paths()).to.have.same.members(expectedPaths);
-        expect(parser.$refs.values()).to.have.keys(expectedPaths);
-      });
-  });
+  //       // The schema path should be the current directory, and all other paths should be absolute
+  //       var expectedPaths = [
+  //         'http://www.example.com/',
+  //         path.abs('specs/object-source/definitions/definitions.json'),
+  //         path.abs('specs/object-source/definitions/name.yaml'),
+  //         path.abs('specs/object-source/definitions/required-string.yaml')
+  //       ];
+  //       expect(parser.$refs.paths()).to.have.same.members(expectedPaths);
+  //       expect(parser.$refs.values()).to.have.keys(expectedPaths);
+  //     });
+  // });
 });
